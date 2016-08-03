b7b06ea1deae62e96f4dd75c5c2a579350c4442e
good7zzang <linux_epoll@naver.com>
2016-08-03 17:27:34 +0900

    123
    123

--- moldcondition.h
+++ moldcondition.h
@@ -6,12 +6,15 @@
 #include <QSqlQuery>
 #include <QDebug>
 #include "simplecrypt.h"
+#include <QVector>
+#include <QLineEdit>
 
-#define CHGSTEP 4
-#define SUCKBACKSTEP 2
-#define HEATER 8
-#define TEMPERATURE 12
-
+#define MAX_INJSTEP 10 //최대 사출단수
+#define MAX_HOLDSTEP 5 //최대 보압단수
+#define CHGSTEP 4 //계량단수
+#define SUCKBACKSTEP 2 //강제후퇴 단수
+#define HEATER 8 //히터갯수
+#define TEMPERATURE 12 //외부온도 갯수
 
 namespace Ui {
 class MoldCondition;
@@ -24,10 +27,18 @@ class MoldCondition : public QMainWindow
 public:
     explicit MoldCondition(QString machine_name,QWidget *parent = 0);
     QString machine_name;
+    QVector<QLineEdit *> injspd_Lalist, injprs_Lalist, injpos_Lalist, holdspd_Lalist, holdprs_Lalist
+                         ,holdtime_Lalist, chgspd_Lalist, chgbps_Lalist, chgpos_Lalist, heater_Lalist,
+                          temperature_Lalist;
+
     ~MoldCondition();
     void init();
     void Execute_Query();
-    void Display(QString injpos[], QString *sovpos);
+    void Display(QString injstep, QString holdstep ,QStringList injspd, QStringList injprs,
+                 QStringList injpos, QString sovpos, QString sovprs, QString sovtime, QString injdeltime,
+                 QStringList holdspd, QStringList holdprs, QStringList holdtime, QStringList chgspd, QStringList chgbps,
+                 QStringList chgpos, QString cooltime, QString chgdeltime, QStringList suckbackpos, QStringList suckbackspd,
+                 QStringList heater, QStringList temperature);
     void closeEvent(QCloseEvent *event);
 
 public:
